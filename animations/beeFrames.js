80dd18c6dd7d96b5eaaa2ddf7c0cc3a8f370b359
LoopHive <197606276+LoopHive@users.noreply.github.com>
2025-10-23 18:12:03 +0330

    Update beeFrames.js

--- animations/beeFrames.js
+++ animations/beeFrames.js
@@ -1,36 +1,34 @@
-const chalk = require('chalk');
-
 const beeFrames = [
-  chalk.yellow(`
+  `
     (\\     /)
      \\_\\_//
     (  •.• )
      > ^ <
-  `),
-  chalk.yellowBright(`
+  `,
+  `
     (\\     /)
      (o_o)
     (  •.• )
      > ^ <
-  `),
-  chalk.yellow(`
+  `,
+  `
     (\\    /)
     ( •.• )
     (  o_o )
      > ^ <
-  `),
-  chalk.yellowBright(`
+  `,
+  `
     (\\    /)
      \\_o_//
     (  •.• )
      > ^ <
-  `),
-  chalk.yellow(`
+  `,
+  `
     (\\     /)
      \\_\\_//
     (  •.• )
      > ^ <
-  `),
+  `,
 ];
 
 module.exports = beeFrames;
